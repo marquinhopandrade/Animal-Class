0dc64cff97f47d0ac91a3c02ef634d056c4f2e93
Marcos Andrade <Marcos Andrade@DESKTOP-8CQOQ8V>
2022-11-01 09:28:20 -0300

    Adicionar arquivos de projeto.

---
+++ Virtual And Override Keywords/Virtual And Override Keywords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Virtual_And_Override_Keywords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
